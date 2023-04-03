1ba357f94531ed7b2bcfe9428b7ee93c6481953e
ColdBlood237 <115800548+ColdBlood237@users.noreply.github.com>
2023-04-03 16:19:50 +0200

    Implement the function that check the outcome of the game

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ const game_board = (() => {
     }
   };
 
-  return { board, get_board, place_mark };
+  const check_if_full = () => {
+    let is_full = true;
+    board.forEach((cell) => {
+      if (cell === "") {
+        is_full = false;
+      }
+    });
+    return is_full;
+  };
+
+  return { board, get_board, place_mark, check_if_full };
 })();
 
 const player_factory = (name, mark) => {
@@ -41,19 +51,77 @@ const game_controller = (() => {
 const screen_controller = (() => {
   const turn_message = document.querySelector(".turn-message");
   const board = document.querySelector(".board");
-  const player = game_controller.active_player;
-  turn_message.textContent = `${player.get_name()}'s turn`;
+
+  function print_turn_message() {
+    const player = game_controller.active_player;
+    turn_message.textContent = `${player.get_name()}'s turn`;
+  }
+
+  function print_outcome() {
+    const player = game_controller.active_player;
+    board.removeEventListener("click", handle_clicks);
+    if (check_winner()) {
+      turn_message.textContent = `${player.get_name()} won 🎉`;
+    } else {
+      turn_message.textContent = `Tie 😬`;
+    }
+  }
+
   board.addEventListener("click", handle_clicks);
 
   render_board();
-  return { turn_message };
+  print_turn_message();
+  return { print_turn_message, print_outcome };
 })();
 
 function handle_clicks(e) {
   game_controller.active_player.place_mark(e.target.dataset.index);
-  game_controller.switch_player();
-  screen_controller.turn_message.textContent = `${game_controller.active_player.get_name()}'s turn`;
+  if (check_winner()) {
+    screen_controller.print_outcome();
+  } else {
+    game_controller.switch_player();
+    screen_controller.print_turn_message();
+  }
+  if (game_board.check_if_full() && !check_winner()) {
+    screen_controller.print_outcome();
+  }
   render_board();
+}
+
+function check_winner() {
+  const board = game_board.board;
+  let win = false;
+  let winner_mark = "";
+  for (let i = 0; i < 3; i++) {
+    // check columns
+    if (
+      board[i] === board[i + 3] &&
+      board[i] === board[i + 6] &&
+      board[i] !== ""
+    ) {
+      win = true;
+    }
+  }
+  for (let i = 0; i < 7; i += 3) {
+    // check rows
+    if (
+      board[i] === board[i + 1] &&
+      board[i] === board[i + 2] &&
+      board[i] !== ""
+    ) {
+      win = true;
+    }
+  }
+  if (board[0] === board[4] && board[0] === board[8] && board[0] !== "") {
+    // check lef diagonal
+    win = true;
+  }
+  if (board[2] === board[4] && board[2] === board[6] && board[2] !== "") {
+    // check right diagonal
+    win = true;
+  }
+
+  return win;
 }
 
 function render_board() {
